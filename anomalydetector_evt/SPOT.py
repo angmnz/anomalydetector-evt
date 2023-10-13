93e3aeaf9dcf196c53e924306ffdec4859d38635
angmnz <angiedmc98@gmail.com>
2023-10-13 20:20:22 -0300

    add some extra structure

---
+++ anomalydetector_evt/SPOT.py
@@ -0,0 +1,9 @@
+# Librerías y utils
+
+class SPOT():
+    """La clase SPOT permitirá crear un objeto al cual
+	se le podrá aplicar distintos métodos.
+	Entregar los datos DSPOT.data(data), se podrá pedir
+	la inicialización SPOT.init(q). Luego se aplicará un metodo
+	llamado SPOT.update(nuevo_dato) que reportará la anomalía zq si
+	nuevo_dato > zq."""
