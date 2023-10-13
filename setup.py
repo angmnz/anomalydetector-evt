db586fbd5b91d5170f1504ab07357698cfe92360
angmnz <angiedmc98@gmail.com>
2023-10-12 23:11:04 -0300

    update setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='anomalydetector-evt',
+    version='0.0.1',
+    descripcion='Paquete para detección de anomalías mediante EVT',
+    url='git@github.com:angmnz',
+    author='Angie Mendez',
+    author_email='angiedmc98@gmail.com',
+    license='unlicense',
+    packages=['anomalydetector-evt'],
+    zip_safe=False
+)
+
