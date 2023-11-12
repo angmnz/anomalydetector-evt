dc9974d271cb315420b617d7b3e55b2b2fc7d59b
angmnz <angiedmc98@gmail.com>
2023-11-12 19:15:00 -0300

    Cambio en setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='anomalydetector-evt',
     version='0.0.1',
-    descripcion='Paquete para detección de anomalías mediante EVT',
-    url='git@github.com:angmnz',
+    descripcion='Paquete para detección de anomalías mediante Extrem Value Theory',
+    url='https://github.com/angmnz/anomalydetector-evt',
     author='Angie Mendez',
     author_email='angiedmc98@gmail.com',
     license='unlicense',
-    packages=['anomalydetector-evt'],
+    packages=['numpy', 'scipy', 'pandas'],
     zip_safe=False
 )
 
