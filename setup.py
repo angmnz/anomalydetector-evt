97e4d312d9b9da2679d18560b5e24576bb36d0f4
angmnz <angiedmc98@gmail.com>
2023-11-12 19:33:51 -0300

    actualizar setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup
 setup(
     name='anomalydetector-evt',
     version='0.0.1',
-    descripcion='Paquete para detección de anomalías mediante Extrem Value Theory',
+    description='Paquete para detección de anomalías mediante Extrem Value Theory',
     url='https://github.com/angmnz/anomalydetector-evt',
     author='Angie Mendez',
     author_email='angiedmc98@gmail.com',
     license='unlicense',
-    packages=['numpy', 'scipy', 'pandas'],
+    packages=['anomalydetector_evt'],
+    intall_requires=['numpy', 'scipy', 'pandas']
     zip_safe=False
 )
 
